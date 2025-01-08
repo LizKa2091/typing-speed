a920908499622562a2e0557cdf0c696d2f818b9b
lizka <lizka4231@gmail.com>
2025-01-09 00:09:36 +0300

    added response errors displaying in console

--- scripts/getWikiText.js
+++ scripts/getWikiText.js
@@ -1,22 +1,32 @@
 const getWikiText = async () => {
-    let wrongReq = true;
-    let articleText;
+    const url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts&explaintext&exlimit=1&generator=random';
 
-    while (wrongReq) {
-        const response = await fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts&explaintext&exlimit=1&generator=random');
+    try {
+        let wrongReq = true;
+        let articleText;
 
-        const data = await response.json();
-
-        const pageId = Object.keys(data.query.pages)[0];
-        articleText = data.query.pages[pageId].extract;
-
-        //avoid displaying empty text and text from forum
-        if (articleText !== '' && !articleText.includes('delete') && !articleText.includes('http') && !articleText.includes('pronunciation') && !articleText.includes('talk') && !articleText.includes('edit') && !articleText.split('').filter(char => 'A' <= char.toUpperCase && char.toUpperCase <= 'Z' ).length > 0) {
-            wrongReq = false;
+        while (wrongReq) {
+            const response = await fetch(url);
+            
+            if (!response.ok) {
+                throw new Error('Bad response status');
+            }
+            
+            const data = await response.json();
+    
+            const pageId = Object.keys(data.query.pages)[0];
+            articleText = data.query.pages[pageId].extract;
+    
+            //avoid displaying empty text and text from forum
+            if (articleText !== '' && !articleText.includes('delete') && !articleText.includes('http') && articleText.length >= 300 && !articleText.includes('file') && !articleText.includes('pronunciation') && !articleText.includes('talk') && !articleText.includes('edit') && !articleText.split('').filter(char => 'A' <= char.toUpperCase && char.toUpperCase <= 'Z' ).length > 0) {
+                wrongReq = false;
+            }
         }
+        return articleText.replaceAll('=', '').replaceAll('–', '-').slice(0, 700);
     }
-
-    return articleText.replaceAll('=', '').slice(0, 700);
+    catch (err) {
+        throw new Error(`Failed to fetch article: `, err);
+    }
 }
 
 export default getWikiText;
