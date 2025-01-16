9d5f86e2317056fc77c9d3364235b363e038188e
lizka <lizka4231@gmail.com>
2025-01-16 18:42:51 +0300

    updated composition generation, mobile layout

--- scripts/getComposition.js
+++ scripts/getComposition.js
@@ -9,34 +9,34 @@ const getComposition = async (requestWord) => {
             const res = await fetch(url);
             const data = await res.json();
 
-            if (data.items) {
-                for (let item of data.items) {
-                    let desc = item.volumeInfo.description;
-                    let language = item.volumeInfo.language;
-                    let imageLink = item.volumeInfo.imageLinks.thumbnail;
-                    let authors = item.volumeInfo.authors;
-                    let title = item.volumeInfo.title;
+            if (data.items && data.items.length > 0) {
+                let randomIndex = Math.floor(Math.random() * data.items.length);
+                let item = data.items[randomIndex]
+                let desc = item.volumeInfo.description;
+                let language = item.volumeInfo.language;
+                let imageLink = item.volumeInfo.imageLinks.thumbnail;
+                let authors = item.volumeInfo.authors;
+                let title = item.volumeInfo.title;
     
-                    if (desc && language === 'en' && desc.length >= 500) {
-                       successResult = true;
+                if (desc && language === 'en' && desc.length >= 500) {
+                    successResult = true;
 
-                       let lastSymb = 500;
-                        //if 500th symbol is a letter from en alphabet, the word hasn't finished
-                        if ('a' <= desc[500] && desc[500] <= 'z') {
-                            for (let i=501; i<desc.length; i++) {
+                    let lastSymb = 500;
 
-                                if (!('a' <= desc[i] && desc[i] <= 'z')) {
-                                    lastSymb = i;
-                                    break;
-                                }
+                    if ('a' <= desc[500] && desc[500] <= 'z') {
+                        for (let i=501; i<desc.length; i++) {
+
+                            if (!('a' <= desc[i] && desc[i] <= 'z')) {
+                                lastSymb = i;
+                                break;
                             }
                         }
-
-                       let resultText = desc.slice(0, lastSymb).replaceAll('’', `'`);
-
-                       return [resultText, imageLink, authors, title];
                     }
-                };
+
+                    let resultText = desc.slice(0, lastSymb).replaceAll('’', `'`);
+
+                    return [resultText, imageLink, authors, title];
+                }
             }
             reqLimit--;
         }
